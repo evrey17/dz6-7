00a6ec98722b54a7c5bebb0840bbb977e8a1fd54
evrey17 <aziz.abitov.1987@gmail.com>
2024-09-23 16:59:02 +0600

    Dz5

--- js/lesson.js
+++ js/lesson.js
@@ -40,3 +40,46 @@ const slideTime = () =>{
 }
 
 setInterval(slideTime , 3000)
+
+// Converter 
+const somInput = document.querySelector('#som')
+const usdInput = document.querySelector('#usd')
+const eurInput = document.querySelector("#eur")
+const converter = (element, element2 , element3) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+
+        request.onload = ()=> {
+            const data = JSON.parse(request.response);
+            if(element === " "){
+                element2.value = ' '
+                element3.value = ' '
+            }
+            if (element === som){
+                element2.value = (element.value / data.usd).toFixed(2);
+                element3.value = (element.value / data.eur).toFixed(2)
+            }
+            if (element === usd){
+                element2.value = (element.value * data.usd).toFixed(2);
+                element3.value = (element.value*data.usd/data.eur).toFixed(2)
+            }
+            if (element === eur){
+                element2.value = (element.value / data.eur).toFixed(2)
+                element3.value = (element.value*data.eur/data.usd).toFixed(2)
+            }
+
+        }
+    }
+}
+
+converter(somInput, usdInput , eurInput)
+converter(usdInput, somInput , eurInput)
+converter(eurInput, somInput , usdInput)
+
+
+console.log('Loading...')
+
+
